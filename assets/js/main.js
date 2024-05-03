a174a61921adc0a3eb3288be0a54a675b6657b3e
Gautam Singh Badwal <140250464+GautamSinghBadwal@users.noreply.github.com>
2024-05-04 03:16:53 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+  const countDownDate = new Date("May 25, 2024 00:00:00").getTime();
+
+  const x = setInterval(function() {
+
+    // Get the current date and time
+    const now = new Date().getTime();
+    
+    // Calculate the time remaining
+    const distance = countDownDate - now;
+    
+    // Calculate days, hours, minutes, and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Display the countdown
+    document.getElementById("days").innerHTML = "<span>" + days + "d</span>";
+    document.getElementById("hours").innerHTML = "<span>" + hours + "h</span>";
+    document.getElementById("minutes").innerHTML = "<span>" + minutes + "m</span>";
+    document.getElementById("seconds").innerHTML = "<span>" + seconds + "s</span>";
+    
+    // If the countdown is over, display a message
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("days").innerHTML = "<span>0d</span>";
+      document.getElementById("hours").innerHTML = "<span>0h</span>";
+      document.getElementById("minutes").innerHTML = "<span>0m</span>";
+      document.getElementById("seconds").innerHTML = "<span>0s</span>";
+    }
+  }, 1000);
